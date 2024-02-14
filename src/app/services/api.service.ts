a5bb41406003e61bc509f82d2e95e90d65b569a0
Adrian <149875676+ADRGG@users.noreply.github.com>
2024-02-14 16:26:26 +0100

    Update api.service.ts
    Borradas variables que no se utilizan

--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -12,26 +12,7 @@ export class ApiService {
 
   private readonly baseURL = 'https://rawg-video-games-database.p.rapidapi.com';
 
-  private headers: HttpHeaders;
-
-  private params: HttpParams;
-
-  constructor( private http: HttpClient ) {
-
-    let head = new HttpHeaders();
-
-    head.append( "x-rapidapi-host", "rawg-video-games-database.p.rapidapi.com" );
-    head.append( "x-rapidapi-key", "45f632c4eemsh81a9c692e52deb7p18cd42jsn0cac7a27ace0")
-
-    this.headers = head;
-
-    let par = new HttpParams();
-
-    par.append( "key", "c13d9de91ed24b13abd23ec72a84ddbe");
-
-    this.params = par;
-
-  }
+  constructor( private http: HttpClient ) {}
 
   findStores(): Observable<StoreListI> {
 
